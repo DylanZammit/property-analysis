06fcc136f21a049d60094d2a4d16461afd7bc3ea
dylan <dylanzam@gmail.com>
2021-11-08 18:48:58 +0100

    scrape remax split into chunks

--- scraping/scrape_remax.py
+++ scraping/scrape_remax.py
@@ -1,4 +1,5 @@
 import pdb
+import time
 import requests
 import argparse
 import os
@@ -14,7 +15,9 @@ parser.add_argument('--debug', help='embed after request', action='store_true')
 args = parser.parse_args()
 
 nprops = args.n
-url = 'https://remax-malta.com/api/properties?Residential=true&Commercial=false&ForSale=true&ForRent=false&page=1&Take={}'.format(nprops)
+take_pp = 1000
+npages = nprops//take_pp+1
+url = 'https://remax-malta.com/api/properties?Residential=true&Commercial=false&ForSale=true&ForRent=false&page={}&Take={}'
 card_class = 'property-card--information'
 property_request_url = 'https://laravel.dhalia.com:8000/server.php/api/property?propertyRef='
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
@@ -24,51 +27,61 @@ property_url_base = 'https://remax-malta.com/listings/'
 
 out_csv = 'remax_properties.csv'
 attributes = 'price region locality locality_id type bedrooms bathrooms area int_area ext_area rooms webpage'.split()
+df = {k: [] for k in attributes}
 
 print(f'getting {nprops} requests...')
-r = s.get(url, headers=headers)
-content = BeautifulSoup(r.text, 'lxml').decode()
-data = json.loads(content[content.find('{'):content.rfind('}')+1])['data']
 
-df = {k: [] for k in attributes}
-n_rejected = 0
-for prop in data['Properties']:
-    if args.debug: pdb.set_trace()
-    if prop['TransactionType'] != 'For Sale' or prop['HidePriceFromWeb']: continue
+for page in range(1, int(npages)+1):
+    print(f'Reading page {page}/{npages}...')
+    time.sleep(3)
+    take = take_pp if page < npages else nprops-page*take_pp
+    if take < 10: continue
 
-    intarea = prop['TotalIntArea']
-    extarea = prop['TotalExtArea']
-    totarea = prop['TotalSqm']
-    if intarea+extarea>totarea: totarea = intarea+extarea
-    if intarea == 0: intarea = totarea-extarea
-    if totarea == 0: totarea = intarea+extarea
-    if extarea == 0: extarea = totarea-intarea
-    if intarea+totarea+extarea==0: 
-        print('Property {} has no area'.format(prop['MLS']))
-        n_rejected+=1
-        continue
-    if totarea < 50: 
-        print('Property {} area too small!'.format(prop['MLS']))
-        n_rejected+=1
-        continue
-    if intarea==0:
-        print('Property {} has no interior area!'.format(prop['MLS']))
-        n_rejected+=1
-        continue
+    r = s.get(url.format(page, take), headers=headers)
+    content = BeautifulSoup(r.text, 'lxml').decode()
+    #import pdb; pdb.set_trace()
+    data = json.loads(content[content.find('{'):content.rfind('}')+1])['data']
 
-    df['int_area'].append(intarea)
-    df['ext_area'].append(extarea)
-    df['area'].append(totarea)
-    df['price'].append(prop['Price'])
-    df['region'].append(prop['Province'])
-    df['locality'].append(prop['Town'])
-    df['locality_id'].append(prop['TownId'])
-    df['type'].append(prop['PropertyType'])
-    df['bedrooms'].append(prop['TotalBedrooms'])
-    df['bathrooms'].append(prop['TotalBathrooms'])
-    df['rooms'].append(prop['TotalRooms'])
+    n_rejected = 0
+    for prop in data['Properties']:
+        if args.debug: pdb.set_trace()
+        #if prop['Town'] == 'Gozo - Munxar': pdb.set_trace()
+        if prop['TransactionType'] != 'For Sale' or prop['HidePriceFromWeb']: continue
 
-    df['webpage'].append(os.path.join(property_url_base, prop['MLS']))
+        intarea = prop['TotalIntArea']
+        extarea = prop['TotalExtArea']
+        totarea = prop['TotalSqm']
+        if intarea+extarea>totarea: totarea = intarea+extarea
+        if intarea == 0: intarea = totarea-extarea
+        if totarea == 0: totarea = intarea+extarea
+        if extarea == 0: extarea = totarea-intarea
+        if intarea+totarea+extarea==0: 
+            print('Property {} has no area'.format(prop['MLS']))
+            n_rejected+=1
+            continue
+        if totarea < 50: 
+            print('Property {} area too small!'.format(prop['MLS']))
+            n_rejected+=1
+            continue
+        if intarea==0:
+            print('Property {} has no interior area!'.format(prop['MLS']))
+            n_rejected+=1
+            continue
+
+        df['int_area'].append(intarea)
+        df['ext_area'].append(extarea)
+        df['area'].append(totarea)
+        df['price'].append(prop['Price'])
+        df['region'].append(prop['Province'])
+        df['locality'].append(prop['Town'])
+        df['locality_id'].append(prop['TownId'])
+        df['type'].append(prop['PropertyType'])
+        df['bedrooms'].append(prop['TotalBedrooms'])
+        df['bathrooms'].append(prop['TotalBathrooms'])
+        df['rooms'].append(prop['TotalRooms'])
+
+        df['webpage'].append(os.path.join(property_url_base, prop['MLS']))
+    print('done')
 
 if not args.debug:
     with open(out_csv, 'w+') as f:
